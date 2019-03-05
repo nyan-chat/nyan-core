92ce8fae237732f71cad10d768d6582cfe6b47ff
Delton <dsh0416@gmail.com>
2019-03-05 23:42:41 +0900

    Show openssl version

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,7 @@ pub mod user;
 pub extern "C" fn version() -> &'static str {
    consts::Consts::VERSION
 }
+
+pub extern "C" fn openssl_version() -> &'static str {
+   openssl::version::version()
+}
